64808ededebb86e507b1db0955c5050f5b9edce3
mfzszgs <su_wy@outlook.com>
2015-10-22 12:45:45 +0800

    a

---
+++ wechat/src/main/java/com/scutpress/cloud/wechat/message/Articles.java
@@ -0,0 +1,17 @@
+package com.scutpress.cloud.wechat.message;
+
+public class Articles {
+
+    private Item item;
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+    
+    
+}
+
